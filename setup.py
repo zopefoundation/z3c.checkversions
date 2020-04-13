d30b37275ba00d8e2b69f7dcbf2a1ee87fabd8aa
Jan-Jaap Driessen <jdriessen@minddistrict.com>
2020-04-13 21:34:07 +0200

    Back to development: 1.3

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='z3c.checkversions',
-    version='1.2',
+    version='1.3.dev0',
     description="Find newer package versions on PyPI",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.rst").read()),
