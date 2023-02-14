be3e6f65b3c8dd51f87bab0277760b9aec395f6d
Michael Howitz <mh@gocept.com>
2023-02-14 08:35:05 +0100

    Pin setuptools to a compatible version.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'setuptools >= 8',
+        'setuptools >= 8, < 67.1',
     ],
     python_requires='>=3.7',
     extras_require={
